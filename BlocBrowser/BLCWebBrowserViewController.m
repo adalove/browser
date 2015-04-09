a617f3eb962aeed94879e27f483370a5d899e8cd
Shefali Friesen <musictechgirl@gmail.com>
2015-04-08 21:05:05 -0400

    functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.m
@@ -0,0 +1,37 @@
+//
+//  BLCWebBrowserViewController.m
+//  BlocBrowser
+//
+//  Created by Shefali Kumar Friesen on 3/26/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "BLCWebBrowserViewController.h"
+
+@interface BLCWebBrowserViewController ()
+
+@end
+
+@implementation BLCWebBrowserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
